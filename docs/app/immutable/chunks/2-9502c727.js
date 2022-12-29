a5903b0a21212887b042a8ed11a999f7336b5d9e
Martin Havala <martin.havala@gmail.com>
2022-12-30 00:26:33 +0100

    publish

---
+++ docs/app/immutable/chunks/2-9502c727.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-bffe94d6.js";import{default as t}from"../components/pages/_page.svelte-9af61453.js";export{t as component,r as universal};
--- docs/app/immutable/chunks/3-fbdbb249.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-bcf95c2b.js";import{default as t}from"../components/pages/bake/_page.svelte-a5e22563.js";export{t as component,r as universal};
