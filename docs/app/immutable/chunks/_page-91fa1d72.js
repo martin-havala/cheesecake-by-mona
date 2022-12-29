d0ee24a2a431df41dd63a7ca312f8956dcd479bd
Martin Havala <martin.havala@gmail.com>
2022-12-29 16:33:00 +0100

    prerender ./bake

--- docs/app/immutable/chunks/_page-91fa1d72.js
+++
@@ -1,1 +0,0 @@
-const e=!0,r=!0,s=!1,t=Object.freeze(Object.defineProperty({__proto__:null,csr:!0,ssr:!0,prerender:!1},Symbol.toStringTag,{value:"Module"}));export{t as _,e as c,s as p,r as s};
---
+++ docs/app/immutable/modules/pages/bake/_page.ts-d345e48e.js
@@ -0,0 +1,1 @@
+import{c as e,p as a,s as p}from"../../../chunks/_page-bcf95c2b.js";export{e as csr,a as prerender,p as ssr};
