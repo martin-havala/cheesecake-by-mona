1a7c120aeabb67f0d4b3885173e7a741842a5cc5
Martin Havala <martin.havala@gmail.com>
2023-01-03 10:09:03 +0100

    responsivity improvements

--- docs/app/immutable/chunks/4-d32feed3.js
+++ docs/app/immutable/chunks/4-d32feed3.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-75379c46.js";import{default as t}from"../components/pages/byMona/_page.svelte-84f38170.js";export{t as component,r as universal};
+import{_ as r}from"./_page-fcb137e8.js";import{default as t}from"../components/pages/byMona/_page.svelte-84f38170.js";export{t as component,r as universal};
