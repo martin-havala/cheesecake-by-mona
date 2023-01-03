16577303ded970ba2676520b90b1a2246c26056e
Martin Havala <martin.havala@gmail.com>
2023-01-03 19:13:11 +0100

    improved stroke fallback; fixed fillings

---
+++ docs/app/immutable/chunks/3-c65e69fe.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-fcb137e8.js";import{default as t}from"../components/pages/bake/_page.svelte-d134eb47.js";export{t as component,r as universal};
--- docs/app/immutable/chunks/4-5b837e69.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-75379c46.js";import{default as t}from"../components/pages/logo/_page.svelte-8cd213d0.js";export{t as component,r as universal};
